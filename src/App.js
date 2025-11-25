cc9b9cd06039dcffb6a805bcf45c08e6301ec3e3
Aferistexe <andrey.ivanov377@gmail.com>
2025-11-26 02:18:21 +0300

    Сделал объект с товарами

--- src/App.js
+++ src/App.js
@@ -2,22 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const catalog =  [
+  {
+    id: 1,
+    name: "Эспрессо",
+    price: 180,
+    image: "/images/espresso.jpg"
+  },
+  {
+    id: 2,
+    name: "Капучино", 
+    price: 220,
+    image: "/images/cappuccino.jpg"
+  },
+  {
+    id: 3,
+    name: "Латте",
+    price: 240,
+    image: "/images/latte.jpg"
+  },
+  {
+    id: 4,
+    name: "Американо",
+    price: 190,
+    image: "/images/americano.jpg"
+  },
+  {
+    id: 5,
+    name: "Моккачино",
+    price: 260,
+    image: "/images/mochaccino.jpg"
+  },
+  {
+    id: 6, 
+    name: "Флэт Уайт",
+    price: 230,
+    image: "/images/flat-white.jpg"
+  },
+  {
+    id: 7,
+    name: "Раф кофе",
+    price: 280,
+    image: "/images/raf-coffee.jpg"
+  },
+  {
+    id: 8,
+    name: "Колд брю",
+    price: 210,
+    image: "/images/cold-brew.jpg"
+  }
+];
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
     </div>
   );
 }
